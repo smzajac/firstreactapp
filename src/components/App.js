5171728c983cf9eda1adbb58e410dd2ab096486a
smzajac <sarahmzajac@gmail.com>
2017-09-13 15:11:20 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          <p>Earthquakes!</p>
+          </div>
+          <div className="my-header">
+
+          <h2>Welcome to React</h2>
+
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
